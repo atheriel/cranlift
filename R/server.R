3bd15d785c2ac35d2e21344ed10dd991137a02bf
Aaron Jacobs <atheriel@gmail.com>
2019-12-20 09:59:33 -0500

    Adds support for serving/PUTing/DELETEing binary packages.
    This does not extend to POST support, because I'm not quite sure how
    this should work yet.

--- R/server.R
+++ R/server.R
@@ -41,9 +41,10 @@ serve <- function(repo, repo_name = "Cranium", host = "127.0.0.1", port = 8000,
 
   # Keep the package index in memory.
   env <- new.env(FALSE, size = 1L)
-  env$index <- readRDS(
-    file.path(contrib.url(repo$path, type = "source"), "PACKAGES.rds")
-  )
+  env$index <- lapply(repo$contrib_urls, function(url) {
+    readRDS(file.path(url, "PACKAGES.rds"))
+  })
+  names(env$index) <- repo$contrib_urls
 
   if (!detach) {
     httpuv::runServer(host, port, list(
@@ -68,10 +69,14 @@ router <- function(repo, config, env) {
     # Serve the index out of memory so that we can be sure it is always up-to-
     # date vis-a-vis this server.
     if (req$REQUEST_METHOD %in% c("GET", "HEAD") &&
-        path == "/src/contrib/PACKAGES.rds") {
+          grepl("PACKAGES.rds$", path)) {
+      location <- file.path(repo, sub("^/", "", path))
+      if (!dirname(location) %in% names(env$index)) {
+        return(not_found())
+      }
       if (req$REQUEST_METHOD == "GET") {
         # NOTE: We're not using the traditional compression here.
-        body <- serialize(env$index, connection = NULL)
+        body <- serialize(env$index[[dirname(location)]], connection = NULL)
       } else {
         body <- raw(0)
       }
@@ -87,12 +92,12 @@ router <- function(repo, config, env) {
 
     # No support for pre-3.0 clients, i.e. raw PACKAGES or PACKAGES.gz files.
     if (req$REQUEST_METHOD %in% c("GET", "HEAD") &&
-        grepl("src/contrib/PACKAGES(|.gz)$", path)) {
+          grepl("PACKAGES(|.gz)$", path)) {
       return(bad_request("Pre-3.0 clients are not supported."))
     }
 
     # Here's a nickel kid, use a real web server instead.
-    if (req$REQUEST_METHOD %in% c("GET", "HEAD") && grepl("^/src", path)) {
+    if (req$REQUEST_METHOD %in% c("GET", "HEAD")) {
       location <- file.path(repo, sub("^/", "", path))
       res <- if (file.exists(location)) {
         size <- file.info(location)[, "size"]
@@ -106,8 +111,8 @@ router <- function(repo, config, env) {
         list(
           status = 200L,
           headers = list(
-            # This is what CRAN uses as a mime type for src packages.
-            "Content-Type" = "x-gzip",
+            # This is what CRAN uses as the mime type.
+            "Content-Type" = if (endsWith(location, "zip")) "zip" else "x-gzip",
             # Send the real content length for HEAD requests.
             # See: https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.13
             "Content-Length" = size
@@ -167,7 +172,9 @@ router <- function(repo, config, env) {
           basename(location), dir = dirname(location), fields = config$fields
         )
         # Update the in-memory representation.
-        env$index <- readRDS(file.path(dirname(location), "PACKAGES.rds"))
+        env$index[[dirname(location)]] <- readRDS(
+          file.path(dirname(location), "PACKAGES.rds")
+        )
         list(
           status = 201L,
           headers = list(
@@ -180,9 +187,10 @@ router <- function(repo, config, env) {
       return(res)
     }
 
-    if (req$REQUEST_METHOD == "PUT" && grepl("^/src", path)) {
+    if (req$REQUEST_METHOD == "PUT") {
       location <- file.path(repo, sub("^/", "", path))
-      if (dirname(location) != contrib.url(repo, type = "source")) {
+      if (!dirname(location) %in% names(env$index)) {
+        # TODO: Allow for creating valid new contrib URLs with an empty index.
         return(bad_request("URI does not match the repository structure."))
       }
 
@@ -209,7 +217,11 @@ router <- function(repo, config, env) {
       }
       pkg <- as.data.frame(t(pkg), stringsAsFactors = FALSE)
 
-      bundle <- sprintf("%s_%s.tar.gz", pkg$Package, pkg$Version)
+      bundle <- sprintf(
+        "%s_%s.%s", pkg$Package, pkg$Version,
+        # Match the existing extension.
+        sub("(.*)\\.(tar\\.gz|zip|tgz)", "\\2", parsed$file$filename)
+      )
       if (basename(location) != bundle) {
         return(bad_request("URI does not match the upload contents."))
       }
@@ -221,7 +233,9 @@ router <- function(repo, config, env) {
         basename(location), dir = dirname(location), fields = config$fields
       )
       # Update the in-memory representation.
-      env$index <- readRDS(file.path(dirname(location), "PACKAGES.rds"))
+      env$index[[dirname(location)]] <- readRDS(
+        file.path(dirname(location), "PACKAGES.rds")
+      )
       return(list(
         status = status,
         headers = list(
@@ -232,7 +246,7 @@ router <- function(repo, config, env) {
       ))
     }
 
-    if (req$REQUEST_METHOD == "DELETE" && grepl("^/src", path)) {
+    if (req$REQUEST_METHOD == "DELETE") {
       location <- file.path(repo, sub("^/", "", path))
 
       if (grepl("PACKAGES", location, fixed = TRUE)) {
@@ -260,7 +274,9 @@ router <- function(repo, config, env) {
         } else {
           cranlike::remove_PACKAGES(basename(location), dirname(location))
           # Update the in-memory representation.
-          env$index <- readRDS(file.path(dirname(location), "PACKAGES.rds"))
+          env$index[[dirname(location)]] <- readRDS(
+            file.path(dirname(location), "PACKAGES.rds")
+          )
         }
 
         list(
--- tests/testthat/test-index.R
+++ tests/testthat/test-index.R
@@ -7,5 +7,21 @@ testthat::test_that("The server produces a valid PACKAGES.rds for src/contrib", 
   )
   testthat::expect_gte(nrow(pkgs), 0)
 
+  pkgs <- available.packages(
+    repos = sprintf("127.0.0.1:%d/", port), type = "win.binary"
+  )
+  testthat::expect_gte(nrow(pkgs), 0)
+
+  pkgs <- available.packages(
+    repos = sprintf("127.0.0.1:%d/", port), type = "mac.binary"
+  )
+  testthat::expect_gte(nrow(pkgs), 0)
+
+  # Bad R version.
+  response <- httr::GET(
+    sprintf("127.0.0.1:%d/bin/windows/2.6/PACKAGES.rds", port), httr::timeout(3)
+  )
+  testthat::expect_equal(httr::status_code(response), 404)
+
   p$kill()
 })
